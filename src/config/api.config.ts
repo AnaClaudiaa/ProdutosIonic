5b329f736215e7d1363430481d8c3d899ef1a025
AnaClaudiaa <aninhardgs19@gmail.com>
2018-10-15 10:33:26 -0300

    Produtos Mockados

---
+++ src/config/api.config.ts
@@ -0,0 +1,10 @@
+import { Produto } from "../model/produto";
+export const PRODUTOS : Produto[] = [
+    {id : 1, nome : "Celular LG K10", preco :799.00, imagem : "k10.jpg"},
+    {id : 2, nome : "Celular Motorola Moto X", preco : 1200.00, imagem : "motox.jpg"},
+    {id : 3, nome : "Celular Samsung S9", preco : 3799.00, imagem : "s9.jpg"},
+    {id : 4, nome : "Celular LG k4", preco : 499.00, imagem : "k4.jpg"},
+    {id : 5, nome : "Celular Samsung J5", preco : 999.00, imagem : "J5.jpg"},
+    {id : 6, nome : "Celular Motorola One", preco : 1400.00, imagem : "motoone.jpg"},
+    
+]
